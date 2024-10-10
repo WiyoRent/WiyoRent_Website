c5ab5d8108307fa8d817303e44d1f9669eb75316
Winston Nji <w.nji1@alustudent.com>
2024-10-10 20:17:13 +0200

    Edited visible listings and status

--- data-structures/featured-house-data.js
+++ data-structures/featured-house-data.js
@@ -1,54 +1,55 @@
 export const featured_houses = [
     
-    {
-        id:"2",
+    {   
+        id:"11",
         houseImg:{
                     Outdoor:[
-                        "/home images/House 2/OutdoorView.jpg"
+                        "/home images/House 11/OutdoorView.jpg"
                     ],
                     LivingRoom: [
-                        "/home images/House 2/LivingRoom.jpg" 
+                        "/home images/House 11/LivingRoom.jpg"
                     ],
                     Kitchen: [
-                        "/home images/House 2/Kitchen.jpg"
+                        "/home images/House 11/Kitchen.jpg"
                     ],
                     Bedroom: [
-                        "/home images/House 2/Bedroom1.jpg"
+                        "/home images/House 11/Bedroom1.jpg"
                     ],
                     Bathroom:[
-                        "/home images/House 2/Bedroom1.jpg"
+                        "/home images/House 11/Bathroom1.jpg"
                     ]  
                 },
 
         house_overview: {
-                            house_name: '1-Bedroom House For Rent In Sekimondo',
-                            house_price: 160000,
+                            house_name: '1-Bedroom Studio For Rent In Masoro',
+                            house_price: 300000,
                             house_commission: 10,
                             status: 'Available',
                             bedrooms:1,
                             bathrooms:1,
-                            distance:6,
-                            property_type:'Apartment',
-                            advert_type: "Normal",
-                            security_deposit: 160000,
+                            distance:10,
+                            property_type:'Studio',
+                            advert_type: 'Featured',
+                            security_deposit: 250000,
                             security_deposit_status: 'Required',
                             minimun_stay_period: 2,
-                            booking_fee: 120000
+                            booking_fee: 250000,
                         },
-        house_amenities: {
-                            Furnished:'NO',
-                            Electricity_Included:'N0',
+        house_amenities:{
+                            Furnished:'YES',
+                            Electricity_Included:'NO',
                             Water_Included:'YES',
-                            WIFI_Included:'N0',
-                            Water_Heater: 'NO',
+                            WIFI_Included:'YES',
+                            Water_Heater: 'YES',
                             Microwave: 'NO',
                             Blender: 'NO',
                             Oven: 'NO',
-                            Kitchen_Utensils: 'NO',
-                            Cooking_Utensils: 'NO',
+                            Kitchen_Utensils: 'YES',
+                            Cooking_Utensils: 'YES',
                             TV: 'NO',
                             Fan: 'NO',
                             Security: 'YES',
+                            Washing_Machine: 'NO',
                             Extra_Services: 'NONE'
                         }
     },
@@ -56,7 +57,7 @@ export const featured_houses = [
         id:"10",
         houseImg:{
                     Outdoor:[
-                        "/home images/House 10/OutdoorView.jpg"
+                        "/home images/House 10/LivingRoom.jpg"
                     ],
                     LivingRoom: [
                         "/home images/House 10/LivingRoom.jpg" 
@@ -70,14 +71,15 @@ export const featured_houses = [
                     ],
                     Bathroom:[
                         "/home images/House 10/Bathroom1.jpg"
-                    ]  
+                    ],
+                    
                 },
 
         house_overview: {
                             house_name: '3-Bedroom Studio For Rent In Zindiro',
                             house_price: 200000,
                             house_commission: 10,
-                            status: 'Available',
+                            status: 'Available (Oct 16)',
                             bedrooms:3,
                             bathrooms:3,
                             distance:10,
@@ -107,43 +109,44 @@ export const featured_houses = [
                         }
     },
     {   
-        id:"5",
+        id:"4",
         houseImg:{
                     Outdoor:[
-                        "/home images/House 5/OutdoorView.jpg"
+                        "/home images/House 4/OutdoorView.jpg"
                     ],
                     LivingRoom: [
-                        "/home images/House 5/LivingRoom.jpg" 
+                        "/home images/House 4/LivingRoom.jpg",
+                        "/home images/House 4/LivingRoom1.jpg" 
                     ],
                     Kitchen: [
-                        "/home images/House 5/Kitchen.jpg"
+                        "/home images/House 4/Bedroom1.jpg"
                     ],
                     Bedroom: [
-                        "/home images/House 5/Bedroom1.jpg"
+                        "/home images/House 4/Bedroom1.jpg"
                     ],
                     Bathroom:[
-                        "/home images/House 5/Bathroom1.jpg"
+                        "/home images/House 4/Bathroom1.jpg"
                     ]  
                 },
 
         house_overview: {
-                            house_name: '1-Bedroom House For Rent In Bumbogo',
-                            house_price: 265000,
+                            house_name: '2-Bedroom Studio For Rent In Masoro',
+                            house_price: 250000,
                             house_commission: 10,
                             status: 'Available',
-                            bedrooms:1,
-                            bathrooms:1,
-                            distance:5,
-                            property_type:'Residential Compound',
+                            bedrooms:2,
+                            bathrooms:2,
+                            distance:10,
+                            property_type:'House',
                             advert_type: 'Featured',
-                            security_deposit: 0,
-                            security_deposit_status: 'Not-Required',
-                            minimun_stay_period: 1,
-                            booking_fee: 132000
+                            security_deposit: 250000,
+                            security_deposit_status: 'Required',
+                            minimun_stay_period: 2,
+                            booking_fee: 250000,
                         },
         house_amenities:{
                             Furnished:'YES',
-                            Electricity_Included:'YES',
+                            Electricity_Included:'NO',
                             Water_Included:'YES',
                             WIFI_Included:'YES',
                             Water_Heater: 'YES',
@@ -152,67 +155,67 @@ export const featured_houses = [
                             Oven: 'YES',
                             Kitchen_Utensils: 'YES',
                             Cooking_Utensils: 'YES',
-                            TV: 'NO',
+                            TV: 'YES',
                             Fan: 'NO',
-                            Security: 'NO',
+                            Security: 'YES',
                             Washing_Machine: 'NO',
                             Extra_Services: 'NONE'
                         }
     },
-    {
-        id:"1",
-        houseImg:{
-                    Outdoor:[
-                        "/home images/House 1/OutdoorView.jpg",
-                        "/home images/House 1/OutdoorView1.jpg"
-                    ],
-                    LivingRoom: [
-                        "/home images/House 1/LivingRoom.jpg" 
-                    ],
-                    Kitchen: [
-                        "/home images/House 1/Kitchen.jpg"
-                    ],
-                    Bedroom: [
-                        "/home images/House 1/Bedroom1.jpg"
-                    ],
-                    Bathroom:[
-                        "/home images/House 1/Bedroom1.jpg"
-                    ]  
-                },
+    // {
+    //     id:"1",
+    //     houseImg:{
+    //                 Outdoor:[
+    //                     "/home images/House 1/OutdoorView.jpg",
+    //                     "/home images/House 1/OutdoorView1.jpg"
+    //                 ],
+    //                 LivingRoom: [
+    //                     "/home images/House 1/LivingRoom.jpg" 
+    //                 ],
+    //                 Kitchen: [
+    //                     "/home images/House 1/Kitchen.jpg"
+    //                 ],
+    //                 Bedroom: [
+    //                     "/home images/House 1/Bedroom1.jpg"
+    //                 ],
+    //                 Bathroom:[
+    //                     "/home images/House 1/Bedroom1.jpg"
+    //                 ]  
+    //             },
 
-        house_overview: {
-                            house_name: '5-Bedroom House For Rent In Kanombe',
-                            house_price: 180000,
-                            house_commission: 10,
-                            status: 'Available',
-                            bedrooms:5,
-                            bathrooms:3,
-                            distance:20,
-                            property_type:'House',
-                            advert_type: "Partnered",
-                            security_deposit:200000,
-                            security_deposit_status: 'Required',
-                            minimun_stay_period: 1,
-                            booking_fee: 450000
-                        },
-        house_amenities: {
-                            Furnished:'YES',
-                            Electricity_Included:'NO',
-                            Water_Included:'YES',
-                            WIFI_Included:'YES',
-                            Water_Heater: 'YES',
-                            Microwave: 'YES',
-                            Blender: 'YES',
-                            Oven: 'YES',
-                            Kitchen_Utensils: 'YES',
-                            Cooking_Utensils: 'YES',
-                            TV: 'YES',
-                            Fan: 'NO',
-                            Washing_Machine: 'YES',
-                            Security: 'YES',
-                            Extra_Services: 'NONE'
-        }
+    //     house_overview: {
+    //                         house_name: '5-Bedroom House For Rent In Kanombe',
+    //                         house_price: 180000,
+    //                         house_commission: 10,
+    //                         status: 'Available',
+    //                         bedrooms:5,
+    //                         bathrooms:3,
+    //                         distance:20,
+    //                         property_type:'House',
+    //                         advert_type: "Partnered",
+    //                         security_deposit:200000,
+    //                         security_deposit_status: 'Required',
+    //                         minimun_stay_period: 1,
+    //                         booking_fee: 450000
+    //                     },
+    //     house_amenities: {
+    //                         Furnished:'YES',
+    //                         Electricity_Included:'NO',
+    //                         Water_Included:'YES',
+    //                         WIFI_Included:'YES',
+    //                         Water_Heater: 'YES',
+    //                         Microwave: 'YES',
+    //                         Blender: 'YES',
+    //                         Oven: 'YES',
+    //                         Kitchen_Utensils: 'YES',
+    //                         Cooking_Utensils: 'YES',
+    //                         TV: 'YES',
+    //                         Fan: 'NO',
+    //                         Washing_Machine: 'YES',
+    //                         Security: 'YES',
+    //                         Extra_Services: 'NONE'
+    //     }
         
-    }
+    // }
     ];
     
